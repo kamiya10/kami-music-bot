d7a47019f134332f0e249f88c057ed4864576bbd
Kamiya <58339640+kamiya10@users.noreply.github.com>
2024-08-31 23:08:54 +0800

    remove dependency report

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,7 @@
 import "dotenv/config";
 
 import { KamiClient } from "@/core/KamiClient";
-import { generateDependencyReport } from "@discordjs/voice";
 import { setConsoleTitle } from "@/utils/console";
-
-console.log(generateDependencyReport());
 
 const client = new KamiClient();
 
